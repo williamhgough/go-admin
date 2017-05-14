4d41c648618b0c499c742a7fc8107d774fec8e7a
William Gough <williamhenrygough@gmail.com>
2017-05-14 20:04:09 +0100

    Refactor to use echo

--- helpers/database.go
+++ helpers/database.go
@@ -6,31 +6,47 @@ import (
 )
 
 // QueryDb - Execute sql statements through helper
-func QueryDb(db *sql.DB, query string, args []string) {
+func QueryDb(db *sql.DB, method string, query string, args map[string]string) sql.Result {
 	// Create table query
 	stmt, err := db.Prepare(query)
 	CheckErr(err, "")
 
-	_, err = stmt.Exec(args)
+	// Create values interface slice
+	values := make([]interface{}, 0, len(args))
+
+	// Check for args
+	if args != nil {
+		for key := range args {
+			values = append(values, args[key])
+		}
+		res, err := stmt.Exec(values...)
+		// Exit if something goes wrong with our SQL statement above
+		CheckErr(err, "")
+
+		// Log successful query
+		fmt.Printf("%s query completed.\n", method)
+
+		return res
+	}
+
+	res, err := stmt.Exec()
 	// Exit if something goes wrong with our SQL statement above
 	CheckErr(err, "")
+
+	// Log successful query
+	fmt.Printf("%s query completed.\n", method)
+
+	return res
+
 }
 
 // InitDB - Initialise DB connection
 func InitDB(filepath string) {
 	db, err := sql.Open("sqlite3", filepath)
-	CheckErr(err, "")
+	CheckErr(err, "Can't open the database.")
 
-	// Create table query
-	stmt, err := db.Prepare("CREATE TABLE IF NOT EXISTS users( id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,fname VARCHAR NOT NULL, lname VARCHAR NOT NULL, password VARCHAR NOT NULL, username VARCHAR NOT NULL);")
-	CheckErr(err, "")
-
-	_, err = stmt.Exec()
-	// Exit if something goes wrong with our SQL statement above
-	CheckErr(err, "")
-
-	fmt.Println("Created Users Table.")
-
+	// Create Users Table
+	QueryDb(db, "Create", "CREATE TABLE IF NOT EXISTS users( id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,fname VARCHAR NOT NULL, lname VARCHAR NOT NULL, username VARCHAR NOT NULL, password VARCHAR NOT NULL);", nil)
 	// If we don't get any errors but somehow still don't get a db connection
 	// we exit as well
 	if db == nil {
--- main.go
+++ main.go
@@ -1,28 +1,19 @@
 package main
 
 import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/williamhgough/go-admin/helpers"
-	"gopkg.in/kataras/iris.v6"
-	"gopkg.in/kataras/iris.v6/adaptors/httprouter"
 )
 
 func main() {
 	// Create new database instance
 	helpers.InitDB("storage.db")
 
-	// Create new instance of echo
-	iris := iris.New()
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Static("/", "build")
 
-	// Add HTTPRouter
-	iris.Adapt(httprouter.New())
-
-	// Gzip all responses
-	iris.Config.Gzip = true
-
-	// Serve up build dir
-	iris.StaticWeb("/", "build/")
-
-	// start server on port:8080
-	iris.Listen(":8080")
+	e.Start(":8080")
 }
